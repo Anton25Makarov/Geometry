0c76f60b25cad8487c80ea92aac99acdaf7bf589
Anton Makarov <nbsketmn250799@gmail.com>
2018-10-25 17:12:37 +0300

    Add class Generator to generate ID for Cubes

---
+++ src/main/java/com/epam/geometry/Generator.java
@@ -0,0 +1,15 @@
+package com.epam.geometry;
+
+public class Generator {
+
+    private static final long MIN_VALUE = 0L;
+    private static final long MAX_VALUE = 1000000L;
+    private static long nextID = MIN_VALUE;
+
+    public static long generateID() {
+        if (nextID > MAX_VALUE) {
+            nextID = MIN_VALUE;
+        }
+        return nextID++;
+    }
+}
